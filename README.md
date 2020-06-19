6aca4592cf4fb971efd77c088e650a91d08505bb
josephlee3454 <joseph.lee3454@gmail.com>
2020-06-18 20:32:02 -0700

    made it look pretty

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 ## Game of Greed I
 
-**Authors:** Lee-Roy King, William Koger, Joseph Lee, Roman Sydoruk **Version:** 1.0.0
+**Authors:** Lee-Roy King, William Koger, Joseph Lee, Roman Sydoruk 
+**Version:** 1.0.0
 
 # Description
 
@@ -27,7 +28,7 @@ Game of Greed I is a python command line version of the dice game. To review rul
 
 
 
-# Overview
+# overveiw of tasks
 * Today you’ll begin working in teams on a command line version of the dice game Game of Greed by expanding your understanding of Python standard library.
 
 * Feature Tasks and Requirements
@@ -42,7 +43,6 @@ Game of Greed I is a python command line version of the dice game. To review rul
 * The input to roll_dice is an integer between 1 and 6.
 * The output of roll_dice is a tuple with random values between 1 and 6.
 * The length of tuple must match the argument given to roll_dice method.
-**********************************************
 * Handle banking points
 * Define a Banker class
 * Add a shelf instance method
@@ -52,4 +52,4 @@ Game of Greed I is a python command line version of the dice game. To review rul
 * bank should add any points on the shelf to total and reset shelf to 0.
 * bank output should be the amount of points added to total from shelf.
 * Add a clear_shelf instance method
-* clear_shelf should remove all unbanked points.* 
+* clear_shelf should remove all unbanked points.
--- game_of_greed_1/game_of_greed.py
+++ game_of_greed_1/game_of_greed.py
@@ -1,22 +1,40 @@
 import random
 from collections import Counter
 class GameLogic:
+  """
+  Gamelogic class
+  """
   
-  @staticmethod
+  def __str__(self):
+    return "gamelogic static methods"
 
+  @staticmethod
   def roll_dice(count):
+    """
+    generates random integer for our dice roll
+
+
+    Returns:
+        integer
+    """
     random.randint(1,6)
     output = tuple()
     for i in range(count):
       pass
     return tuple(random.randint(1,6) for _ in range(count))
 
-
-
-    
-
   @staticmethod
-  def calculat_score(dice):
+  def calculat_score(dice: tuple) -> int:
+    """
+    calculates score
+
+    Args:
+        dice (tuple): [description]
+
+    Returns:
+        int: [description]
+    """
+
     total = 0
     if isinstance(dice,int):
       dice = tuple([dice])
@@ -24,31 +42,50 @@ class GameLogic:
     # if len(dice) == 6:
     if len(current_dice) == 6:
       return 1500##straight
+
     elif list(current_dice.values()).count(2) == 3:
       return 1500##pair
     
-
     total = GameLogic.score_triples(total, current_dice)
     total = GameLogic.score_singles(total, current_dice)
     return total
   
   
   @staticmethod
-  def score_triples(total, current_dice):
+  def score_triples(total:int, current_dice:tuple) -> int:
+    """ scores the triple dice values 
+
+    Args:
+        total (int): [description]
+        current_dice (tuple): [description]
+
+    Returns:
+        int: [description]
+    """
    
     for i in range(len(current_dice)):
       current_keys = list(current_dice.keys())
       key1 = current_keys[i]
       key_value = current_dice[current_keys[i]]
-
+      
       if key_value >= 3:
+      
         if key1 == 1:
           key1 = 10
         total += (key1*100) + (key_value -3) * key1 * 100
     return total
 
   @staticmethod
-  def score_singles(total, current_dice):
+  def score_singles(total: int, current_dice: tuple) -> int:
+    """ scores our single dice values
+
+    Args:
+        total (int): [description]
+        current_dice (tuple): [description]
+
+    Returns:
+        int: [description]
+    """
 
     for i in range(len(current_dice)):
       current_keys = list(current_dice.keys())
@@ -56,10 +93,13 @@ class GameLogic:
       key_value = current_dice[current_keys[i]]
     
       if key_value <= 2 and key_value >= 0:
+        
         if key1 == 1:
           total += key_value*100  
+        
         if key1 == 5:
           total += key_value*50
+    
     return total 
 
 
@@ -67,20 +107,45 @@ class GameLogic:
 
 
 class Banker:
+  """
+  banker class
+  
+  """
   def __init__(self):
+    """
+    initilaizes banker class
+
+    """
     self.shelf_storage = 0
     self.banked_points = 0
+
+  def __str__(self):
+    return f"{self.shelf_storage}:{self.banked_points}"
+
+  def __repr__(self):
+    return f"{self.shelf_storage}:{self.banked_points}"
  
-  def shelf(self,value):
+  def shelf(self,value:int) -> None:
+    """
+    shelfs the points
+
+    """
     self.shelf_storage += value
     
 
   def bank(self):
+    """ 
+    clears the shelf after you adde the banked points
+
+    """
     self.banked_points += self.shelf_storage
     self.clear_shelf()
     return self.banked_points
 
 
   def clear_shelf(self):
+    """
+    clears the shelf 
+    """
     self.shelf_storage = 0
 
