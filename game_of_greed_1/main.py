9d988f553e641486584fa31d189ebeea29efb77a
josephlee3454 <joseph.lee3454@gmail.com>
2020-06-20 15:32:45 -0700

    vs code sucks

--- game_of_greed_1/main.py
+++ game_of_greed_1/main.py
@@ -2,39 +2,44 @@ from game_of_greed import GameLogic
 from game_of_greed import Banker
 import sys
 
-
-if __name__ == "__main__":
-    bank = Banker()
-    def main(num_dice):
+def turn(num_dice):
+      bank = Banker()
       while num_dice:
         dice = GameLogic.roll_dice(num_dice)
         print(dice)
-        if GameLogic.calculat_score(dice) == 0:
-          print("zilch")
-          sys.exit()
+        zilch_out(dice)
         user_input = input("what dice do you want: ")
         user_input_list = [int(num) for num in user_input] 
-      
         num_dice, remaining_dice = GameLogic.dice_handler(user_input_list, dice)
         current_score = GameLogic.calculat_score(remaining_dice)
-        if current_score == 0:
-          print("zilch")
-          sys.exit()
+        zilch_out(remaining_dice)
         print("current_score , ", current_score)
         bank.shelf(current_score)
-        bank_user_input = input("Roll again ? : ")
-        while bank_user_input != "y" or bank_user_input != "n":
-          if bank_user_input == "n":
-            bank.bank()
-            print("your banked points " + str(bank.banked_points))
-            sys.exit()
+        play_again(bank)
+
+
+def zilch_out(dice):
+  if not GameLogic.calculat_score(dice):
+    print("zilch")
+    sys.exit()
+
+  
+
+def play_again(bank):
+  bank_user_input = input("Roll again y/n ? : ")
+  while bank_user_input != "y" or bank_user_input != "n":
+    if bank_user_input == "n":
+      bank.bank()
+      print("your banked points " + str(bank.banked_points))
+      sys.exit()
+    
+    elif bank_user_input == "y":
+      print("this is what is on your shelf  " + str(bank.shelf_storage))
+      break 
+    ## TODO: we need this to prevent infinite loop but its fundamentally wrong 
+    else: 
+      bank_user_input = input("Roll again y/n ? : ")
           
-          elif bank_user_input == "y":
-            print("this is what is on your shelf  " + str(bank.shelf_storage))
-            break 
-          ## TODO: we need this to prevent infinite loop but its fundamentally wrong 
-          else: 
-            bank_user_input = input("Roll again ? : ")
-            
-          
-    main(6)
+if __name__ == "__main__":
+    
+  turn(6)
