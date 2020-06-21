41830a74ba5556808050aef353f01f0079a6522e
JB-Tellez <jb.tellez@gmail.com>
2020-06-21 00:33:25 -0700

    junk

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,9 +1,10 @@
 from tests.flow.flo import Flo
 
 def test_flow():
-  assert Flo
-
   Flo.test('tests/flow/wanna_play.txt')
 
-def test_flow_bank_one():
-  Flo.test('tests/flow/bank_one_roll_then_quit.txt')
+def test_bank_one_roll_then_quit():
+    Flo.test('tests/flow/bank_one_roll_then_quit.txt')
+
+def test_flow_do_wanna_play_then_quit():
+  Flo.test('tests/flow/do_wanna_play_then_quit.txt')
